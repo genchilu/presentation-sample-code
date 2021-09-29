eeca1834287d727d0d14c40b15308413914b5d30
Genchi Lu <genchi.lu@TW168020131M.local>
2021-09-29 16:26:29 +0800

    debug

---
+++ concurrency-os-python-java-golang/golang/goroutine-mem/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"os"
+	"runtime/trace"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func readBigFile() {
+	fi, err := os.Open("bigfile")
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := fi.Read(buf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		if 0 == n {
+			break
+		}
+	}
+	wg.Done()
+}
+
+func main() {
+	f, _ := os.Create("trace.out")
+	defer f.Close()
+	_ = trace.Start(f)
+	defer trace.Stop()
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go readBigFile()
+	}
+
+	wg.Wait()
+}
